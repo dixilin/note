054870a94d7d0d2f765a67a1b614ee35cba7aa5e
dixilin <124295108@qq.com>
2021-11-23 19:06:14 +0800

    perf

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.npm_lifecycle_event === 'dev'  ? '/' : process.env.BUILD_ENV === 'github' ? '/note/public' : './'
+const BASE_URL = process.env.npm_lifecycle_event === 'dev'  ? '/' : process.env.BUILD_ENV === 'github' ? '/note/public/' : './'
 
 module.exports = {
   "title": "叉子好像不行",
