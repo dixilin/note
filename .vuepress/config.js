87f41c7b9b9192926e642aaa744669dfb35b7569
dixilin <124295108@qq.com>
2021-11-23 17:18:23 +0800

    perf

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,10 @@
-// const BASE_URL = process.env.npm_lifecycle_event === 'dev' ? '/' : './'
+const BASE_URL = process.env.npm_lifecycle_event === 'dev' ? '/' : './'
 
 module.exports = {
   "title": "叉子好像不行",
   "description": "叉子写给自己看的笔记",
   "dest": "dist",
-  // "base": BASE_URL,
+  "base": BASE_URL,
   "head": [
     [
       "link",
